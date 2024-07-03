63ecd46e7fe115dab00dfde15353435688a9fbab
asieraduriz <asier.aduriz@preply.com>
2024-07-03 19:39:25 +0200

    Started with recurrent transaction picker

--- components/AddTransactionForm/index.tsx
+++ components/AddTransactionForm/index.tsx
@@ -1,75 +1,112 @@
 import { useState } from "react";
 import { Text, TextInput, View } from "../Themed";
 import { Button, StyleSheet, Switch } from "react-native";
-import { Currency, RecurrentTransaction, SingleTransaction, Transaction } from "@/types";
+import {
+  Currency,
+  RecurrentTransaction,
+  SingleTransaction,
+  Transaction,
+} from "@/types";
 import { Picker } from "@react-native-picker/picker";
 import { CurrencyPicker, RecurrencyPickers } from "../Pickers";
 import { Defaults } from "@/constants";
+import { useToggle } from "@/hooks";
 
 export const AddTransactionForm = () => {
-    const [amount, setAmount] = useState<number>(0);
-    const [title, setTitle] = useState("");
-    const [currency, setCurrency] = useState<Currency["code"]>("USD");
-    const [category, setCategory] = useState<Transaction["category"]>("");
-    const [otherCategory, setOtherCategory] = useState<Transaction["category"]>("");
-    const [isRecurring, setIsRecurring] = useState(false);
+  const [amount, setAmount] = useState<number>(0);
+  const [title, setTitle] = useState("");
+  const [currency, setCurrency] = useState<Currency["code"]>("USD");
+  const [category, setCategory] = useState<Transaction["category"]>("");
+  const [otherCategory, setOtherCategory] =
+    useState<Transaction["category"]>("");
+  const [isRecurring, recurringToggle] = useToggle(false);
 
-    const [singleTransactionDate, setSingleTransactionDate] = useState<SingleTransaction["date"]>(Defaults.SingleTransactionDate);
+  const [singleTransactionDate, setSingleTransactionDate] = useState<
+    SingleTransaction["date"]
+  >(Defaults.SingleTransactionDate);
 
-    const [recurrentTransaction, setRecurrentTransaction] = useState<RecurrentTransaction["recurrence"]>(Defaults.FrequencyRecurrence);
+  const [recurrentTransaction, setRecurrentTransaction] = useState<
+    RecurrentTransaction["recurrence"]
+  >(Defaults.FrequencyRecurrence);
 
-    return (
-        <View style={styles.container}>
-            <TextInput style={styles.input} value={title} onChangeText={setTitle} placeholder="Title" />
-            <View style={{ display: 'flex', alignItems: 'center', flexDirection: "row" }}>
-                <View style={{ width: 150, ...styles.input, flex: 1 }}>
-                    <CurrencyPicker currency={currency} setCurrency={setCurrency} />
-                </View>
-                <TextInput style={{ ...styles.input, flex: 1 }} value={`${amount}`} onChangeText={(value) => setAmount(Number(value))} placeholder="Amount" keyboardType="numeric" />
-            </View>
-            <Picker selectedValue={category} onValueChange={setCategory}>
-                <Picker.Item label="Groceries" value="groceries" />
-                <Picker.Item label="Rent" value="rent" />
-                <Picker.Item label="Utilities" value="utilities" />
-                <Picker.Item label="Other" value="other" />
-            </Picker>
-            {category === "other" && (
-                <TextInput style={styles.input} value={otherCategory} onChangeText={setOtherCategory} placeholder="Please add a new category" />
-            )}
-            <View style={styles.switchContainer}>
-                <Text style={isRecurring ? styles.disabledRecurrenceText : undefined}>One-time</Text>
-                <Switch value={isRecurring} onValueChange={setIsRecurring} />
-                <Text style={isRecurring ? undefined : styles.disabledRecurrenceText}>Recurrent</Text>
-            </View>
-            {
-                isRecurring ? (
-                    <RecurrencyPickers.Recurrent recurrence={recurrentTransaction} setRecurrence={setRecurrentTransaction} />
-                ) : (
-                    <RecurrencyPickers.Single occurrence={singleTransactionDate} setRecurrence={setSingleTransactionDate} />
-                )
-            }
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={setTitle}
+        placeholder="Title"
+      />
+      <View
+        style={{ display: "flex", alignItems: "center", flexDirection: "row" }}
+      >
+        <View style={{ width: 150, ...styles.input, flex: 1 }}>
+          <CurrencyPicker currency={currency} setCurrency={setCurrency} />
+        </View>
+        <TextInput
+          style={{ ...styles.input, flex: 1 }}
+          value={`${amount}`}
+          onChangeText={(value) => setAmount(Number(value))}
+          placeholder="Amount"
+          keyboardType="numeric"
+        />
+      </View>
+      <Picker selectedValue={category} onValueChange={setCategory}>
+        <Picker.Item label="Groceries" value="groceries" />
+        <Picker.Item label="Rent" value="rent" />
+        <Picker.Item label="Utilities" value="utilities" />
+        <Picker.Item label="Other" value="other" />
+      </Picker>
+      {category === "other" && (
+        <TextInput
+          style={styles.input}
+          value={otherCategory}
+          onChangeText={setOtherCategory}
+          placeholder="Please add a new category"
+        />
+      )}
+      <View style={styles.switchContainer}>
+        <Text style={isRecurring ? styles.disabledRecurrenceText : undefined}>
+          One-time
+        </Text>
+        <Switch value={isRecurring} onValueChange={recurringToggle.flip} />
+        <Text style={isRecurring ? undefined : styles.disabledRecurrenceText}>
+          Recurrent
+        </Text>
+      </View>
+      {isRecurring ? (
+        <RecurrencyPickers.Recurrent
+          recurrence={recurrentTransaction}
+          setRecurrence={setRecurrentTransaction}
+        />
+      ) : (
+        <RecurrencyPickers.Single
+          occurrence={singleTransactionDate}
+          setRecurrence={setSingleTransactionDate}
+        />
+      )}
 
-            <Button title="Add Transaction" onPress={() => { }} />
-        </View >
-    );
+      <Button title="Add Transaction" onPress={() => {}} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 20,
-    },
-    input: {
-        height: 40,
-        borderColor: "gray",
-        borderWidth: 1,
-    },
-    switchContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 10,
-    },
-    disabledRecurrenceText: {
-        color: 'grey'
-    }
+  container: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  disabledRecurrenceText: {
+    color: "grey",
+  },
 });
--- components/Pickers/RecurrentTransactionPicker.tsx
+++ components/Pickers/RecurrentTransactionPicker.tsx
@@ -1,16 +1,62 @@
-import { RecurrentTransaction } from "@/types";
-import { View } from "../Themed";
+import { useToggle } from "@/hooks";
+import {
+  FrequencyRecurrence,
+  IntervalRecurrence,
+  RecurrentTransaction,
+  TransactionRecurrenceTypes,
+} from "@/types";
+import { StyleSheet, Switch } from "react-native";
+import { Text, View } from "../Themed";
+
+const FrequencyTransactionPicker = ({}: {
+  recurrence: RecurrentTransaction["recurrence"];
+  setRecurrence: (recurrence: FrequencyRecurrence) => void;
+}) => {
+  return <View />;
+};
+
+const IntervalTransactionPicker = ({}: {
+  recurrence: RecurrentTransaction["recurrence"];
+  setRecurrence: (recurrence: IntervalRecurrence) => void;
+}) => {
+  return <View />;
+};
 
 type Props = {
   recurrence: RecurrentTransaction["recurrence"];
   setRecurrence: (recurrence: RecurrentTransaction["recurrence"]) => void;
 };
 
-export const RecurrentTransactionPicker = ({ }: Props) => {
-
+export const RecurrentTransactionPicker = (props: Props) => {
+  const [isFrequency, frequencyToggle] = useToggle(true);
   return (
     <View>
-
+      <View style={styles.switchContainer}>
+        <Text style={isFrequency ? styles.disabledRecurrenceText : undefined}>
+          Interval
+        </Text>
+        <Switch value={isFrequency} onValueChange={frequencyToggle.flip} />
+        <Text style={isFrequency ? undefined : styles.disabledRecurrenceText}>
+          Frequency
+        </Text>
+      </View>
+      {props.recurrence.type === TransactionRecurrenceTypes.frequency ? (
+        <FrequencyTransactionPicker {...props} />
+      ) : (
+        <IntervalTransactionPicker {...props} />
+      )}
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  disabledRecurrenceText: {
+    color: "grey",
+  },
+});
