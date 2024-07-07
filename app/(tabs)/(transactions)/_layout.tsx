f6f71856fe69ebd6b58dac564900cfa543ffb701
Asier Aduriz <aduriz_06@hotmail.com>
2024-07-07 10:21:20 +0200

    Move Add transaction to header

--- app/(tabs)/(transactions)/_layout.tsx
+++ app/(tabs)/(transactions)/_layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Transactions" }} />
+      <Stack.Screen name="index" options={{
+        title: "Transactions", headerRight: () => <Link href="/add">
+          <MaterialCommunityIcons name="wallet-plus" size={24} color="white" />
+        </Link>
+
+      }} />
       <Stack.Screen name="add" options={{ title: "Add transaction" }} />
       <Stack.Screen name="edit/[id]" options={{ title: "Edit transaction" }} />
     </Stack>
--- pages/Transactions/index.tsx
+++ pages/Transactions/index.tsx
@@ -1,9 +1,7 @@
 import { OccurrencesList } from "@/components/OccurrencesList";
-import { Text, View } from "@/components/Themed";
-import { TransactionsList } from "@/components/TransactionsList";
+import { View } from "@/components/Themed";
 import { useTransactions } from "@/contexts";
 import { Transformers } from "@/transformers";
-import { Link } from "expo-router";
 import { FC, useMemo } from "react";
 
 export const TransactionsPage: FC = () => {
@@ -13,9 +11,6 @@ export const TransactionsPage: FC = () => {
   return (
     <View>
       <OccurrencesList occurrences={groupedOccurrences} />
-      <Link href="/add">
-        <Text>Add transaction</Text>
-      </Link>
     </View>
   );
 };
